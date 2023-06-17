405312ad0152d90da7bbc45ce09028354ee43d35
larroquejl@gmail.com <larroquejl@gmail.com>
2023-06-17 10:40:32 -0400

    added innerHTML to allow script output to be stored in the 'password' id.

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,15 @@
-
+// HTML checkbox watchers
 const upperCheckbox = document.querySelector("#uppercase");
 const lowerCheckbox = document.querySelector("#lowercase");
 const specCheckbox = document.querySelector("#specialchar");
 const numCheckbox = document.querySelector("#numeric");
 
+// character set arrays 
 var numeric = ["0","1","2","3","4","5","6","7","8","9"];
 var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var special = ["!","@","#","$","%","^","&","*","(",")","{","}",":","<",">","?","/",".",","];
+var special = ["!","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~",];
 
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -26,6 +26,7 @@ function writePassword() {
   };
 
   var passChar = [];
+// ifs to determine character set used based on user supplied checkboxes. 
 
   if (upperCheckbox.checked && lowerCheckbox.checked && specCheckbox.checked && numCheckbox.checked) {
   window.alert("At least one character type must be used to generate a password.");
@@ -34,103 +35,87 @@ function writePassword() {
 
   else if (upperCheckbox.checked && lowerCheckbox.checked && specCheckbox.checked) {
     passChar = Array.from(numeric);
-    // localStorage.setItem("passChar",charset);
   }
 
   else if (upperCheckbox.checked && lowerCheckbox.checked && numCheckbox.checked) {
     passChar = Array.from(special);
-    // localStorage.setItem("passChar",charset);
   }
 
   else if (upperCheckbox.checked && specCheckbox.checked && numCheckbox.checked) {
     passChar = Array.from(lowercase);
-    // localStorage.setItem("passChar",charset);
   }
 
   else if (lowerCheckbox.checked && specCheckbox.checked && numCheckbox.checked) {
     passChar = Array.from(uppercase);
-    // localStorage.setItem("passChar",charset);
   }
 
   else if (upperCheckbox.checked && lowerCheckbox.checked) {
     passChar = Array.from(special.concat(numeric));
-    // localStorage.setItem("passChar",charset);
   }
 
   else if (upperCheckbox.checked && specCheckbox.checked) {
     passChar = Array.from(lowercase.concat(numeric));
-    // localStorage.setItem("passChar",charset);
   }
 
   else if (upperCheckbox.checked && numCheckbox.checked) {
     passChar = Array.from(lowercase.concat(special));
-    // localStorage.setItem("passChar",charset);
   }
 
   else if (lowerCheckbox.checked && specCheckbox.checked) {
     passChar = Array.from(uppercase.concat(numeric));
-    // localStorage.setItem("passChar",charset);
   }
 
   else if (lowerCheckbox.checked && numCheckbox.checked) {
     passChar = Array.from(uppercase.concat(special));
-    // localStorage.setItem("passChar",charset);
   }
 
   else if (numCheckbox.checked && specCheckbox.checked) {
     passChar = Array.from(uppercase.concat(lowercase));
-    // localStorage.setItem("passChar",charset);
   }
 
   else if (upperCheckbox.checked) {
     passChar = Array.from(lowercase.concat(numeric,special));
-    // localStorage.setItem("passChar",charset);
   }
 
   else if (lowerCheckbox.checked) {
     passChar = Array.from(uppercase.concat(numeric,special));
-    // localStorage.setItem("passChar",charset);
   }
 
   else if (specCheckbox.checked) {
     passChar = Array.from(uppercase.concat(lowercase,numeric));
-    // localStorage.setItem("passChar",charset);
   }
 
   else if (numCheckbox.checked) {
     passChar = Array.from(uppercase.concat(lowercase,special));
-    // localStorage.setItem("passChar",charset);
   }
 
   else {
     passChar = Array.from(uppercase.concat(lowercase,numeric,special));
-    // localStorage.setItem("passChar",charset);
   };
-//console.log(charset);
 
 console.log(passChar.length);
 
-// var passwordText = document.querySelector("#password");
+// loop for randomly assign values for an array of equal length as the user specified. 
 
 var passwordArray = [];
 
 for (var i=0; i < userChoice; i++) {
+
   var index = Math.floor(Math.random() * passChar.length);
+
   var characterChoice = passChar[index];
+
   passwordArray.push(characterChoice); 
-  console.log(passwordArray);   
+   
   };
 
-password = passwordArray.toString();
-password = document.querySelector ("#password");
+var password = passwordArray.join("");
 
+document.getElementById("password").innerHTML = password;
+
+console.log(password);
 }
+
 // Add event listener to generate button
 generateBtn.addEventListener("click",writePassword);
-
-// should be - generate password calls a bunch of other functions which are already defined. 
-
-
-// if statements for charset 
-
 
